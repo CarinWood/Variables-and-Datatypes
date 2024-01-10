5b76bd07375f9b1ccb87088d61f7a6c2203eef22
CarinWood <carin.wood.85@gmail.com>
2024-01-10 12:28:54 +0100

    first commit

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+
+/* Exercise 1 */
+
+let age = 38
+console.log("this is my age: " + age)
+
+/* Exercise 2 */
+
+let name = "Carin"; //det moderna sättet att deklarera en variabel
+var haircolor = "blonde"; //det äldre sättet att deklarera en variabel. Lever över alla scoop
+const city = 'Gothenburg' //Kan ej skrivas över/ändras
+
+console.log('My name is ' + name + ". My haircolor is " + haircolor + " and I live in " + city);
+
+/* Exercise 3 */
