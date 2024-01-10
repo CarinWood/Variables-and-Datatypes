e0275a742465f4e76819a78a480f5bc0aecf9378
CarinWood <carin.wood.85@gmail.com>
2024-01-10 12:32:43 +0100

    exercise 3 finished

--- sandbox.js
+++ sandbox.js
@@ -6,10 +6,20 @@ console.log("this is my age: " + age)
 
 /* Exercise 2 */
 
-let name = "Carin"; //det moderna sättet att deklarera en variabel
+let forname = "Carin"; //det moderna sättet att deklarera en variabel
 var haircolor = "blonde"; //det äldre sättet att deklarera en variabel. Lever över alla scoop
 const city = 'Gothenburg' //Kan ej skrivas över/ändras
 
-console.log('My name is ' + name + ". My haircolor is " + haircolor + " and I live in " + city);
+console.log('My name is ' + forname + ". My haircolor is " + haircolor + " and I live in " + city);
 
 /* Exercise 3 */
+
+let animal = "dog";
+let animalAge = 3;
+let chipped = true;
+
+console.log("animal: " + animal)
+console.log("age: " + animalAge)
+console.log("chipped: " + chipped)
+
+/* Exercise 4 */
